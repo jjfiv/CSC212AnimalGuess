1d1b68bc04a07af233e22be9c399fb382398b75f
John Foley <jjfoley@smith.edu>
2019-11-06 20:54:37 -0500

    Working on starter code

---
+++ src/main/java/edu/smith/cs/csc212/guess/GuessingMain.java
@@ -0,0 +1,8 @@
+package edu.smith.cs.csc212.guess;
+
+public class GuessingMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the guessing game!");
+        System.out.println("To play, first think of an animal.");
+    }
+}
