7671c0d4f59029f8c4debc5aef5372c47a3b930a
John Foley <jjfoley@smith.edu>
2019-11-16 11:33:48 -0500

    update GuessingMain

--- src/main/java/edu/smith/cs/csc212/guess/GuessingMain.java
+++ src/main/java/edu/smith/cs/csc212/guess/GuessingMain.java
@@ -1,7 +1,14 @@
 package edu.smith.cs.csc212.guess;
 
+/**
+ * This is the main file where the whole game play loop goes.
+ *
+ */
 public class GuessingMain {
-
+    /**
+     * 
+     * @param args
+     */
     public static void main(String[] args) {
         DecisionTreeNode knowledge = new DecisionTreeNode("Is it a mammal?", 
             new DecisionTreeNode("Mouse"), 
@@ -16,8 +23,10 @@ public class GuessingMain {
             System.out.println("To play, first think of an animal.");
 
             // TODO: Maybe something like this?
-            // knowledge.play();
+            // knowledge.play(); <- leading toward recursive solution.
+            // playGame(knowledge) <- 
 
+            // TODO: remove this.
             System.out.println("...I guess there's no game here yet...");
 
             if (input.confirm("Do you want to play again?")) {
